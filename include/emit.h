7097a681ada48dc4a1b4023c9fd62f8442e4cde9
alex-unofficial <athanasiadisalex2@gmail.com>
2025-09-16 20:12:37 +0300

    added emit instruction method

--- include/emit.h
+++ include/emit.h
@@ -3,8 +3,15 @@
 
 #include <stdio.h>
 
+#define COLWIDTH 80
+
 extern FILE *mixout;
 
 int emit_line(const char *fmt, ...);
+
+int emit_inst(const char *label,
+              const char *opmnen,
+              const char *addexp,
+              const char *comment);
 
 #endif
--- src/emit.c
+++ src/emit.c
@@ -17,3 +17,22 @@ int emit_line(const char *fmt, ...) {
   fputc('\n', mixout);
   return 0;
 }
+
+int emit_inst(const char *label,
+              const char *opcode,
+              const char *address,
+              const char *comment) {
+
+  if (!mixout) return -1;
+
+  fprintf("%-10s", label ? label : "");
+  fprintf(" %-4s", opcode);
+  fprintf(" %-22s", address ? address : "");
+  if (comment && comment[0]) {
+    fprintf(" ; %s", comment);
+  }
+  fputc('\n', mixout);
+
+  return 0;
+}
+
